550915cfa97a3a409e6f0a2e5e185ed919ac6ac3
theallegrarr <nurudeen.amedu@gmail.com>
2020-01-31 21:19:13 +0100

    refactor: log environment on server start

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const server = require('./api/server.js');
 
 const PORT = process.env.PORT || 3300;
 server.listen(PORT, () => {
-  console.log(`\n=== Server listening on port ${PORT} ===\n`);
+  console.log(`\n=== listening on port ${PORT} in ${process.env.NODE_ENV}===\n`);
 });
